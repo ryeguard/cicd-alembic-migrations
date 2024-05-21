5d518ceb48fbfebfbf9d64b9426a7134670fae69
Simon Rygård <simon.rygard+dev.github@gmail.com>
2024-05-21 19:54:23 +0200

    run: `alembic revision -m  "Add a column"`

---
+++ alembic/versions/75342276a34d_add_a_column.py
@@ -0,0 +1,26 @@
+"""Add a column
+
+Revision ID: 75342276a34d
+Revises: 15042ec21c19
+Create Date: 2024-05-21 19:53:53.663194
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '75342276a34d'
+down_revision: Union[str, None] = '15042ec21c19'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
