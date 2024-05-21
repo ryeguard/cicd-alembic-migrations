4df08d92ec07cc005b32e595e2377a9b89d91ce7
Simon Rygård <simon.rygard+dev.github@gmail.com>
2024-05-21 19:55:11 +0200

    feat: add column to migration

--- alembic/versions/75342276a34d_add_a_column.py
+++ alembic/versions/75342276a34d_add_a_column.py
@@ -5,6 +5,7 @@ Revises: 15042ec21c19
 Create Date: 2024-05-21 19:53:53.663194
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,15 +13,15 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '75342276a34d'
-down_revision: Union[str, None] = '15042ec21c19'
+revision: str = "75342276a34d"
+down_revision: Union[str, None] = "15042ec21c19"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
+    op.add_column("account", sa.Column("last_transaction_date", sa.DateTime))
 
 
 def downgrade() -> None:
-    pass
+    op.drop_column("account", "last_transaction_date")
